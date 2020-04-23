e23f1d1d36f345b8474120db03e44d1edea8a8b5
Timothy <timothyattah733@gmail.com>
2020-04-23 16:15:37 +0100

    added new header

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+class Counter extends Component {
+	render() {
+		return (
+			<div>
+				<h1>React App</h1>
+				<p>Shopping Cart Page</p>
+			</div>
+		);
+	}
+}
+
+export default Counter;
